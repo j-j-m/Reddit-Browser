47260a14f1a69a5898bfae01d930417ce06512e9
Jacob Martin <jacobmartin@rccl.com>
2018-05-14 11:00:00 -0400

    attempt to fix a disagreement with linguist

--- build/Reddit.build/Release-iphoneos/RedditComponents.build/DerivedSources/RedditComponents_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char RedditComponentsVersionString[];
- extern const double RedditComponentsVersionNumber;
-
- const unsigned char RedditComponentsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:RedditComponents  PROJECT:Reddit-1" "\n";
- const double RedditComponentsVersionNumber __attribute__ ((used)) = (double)1.;
